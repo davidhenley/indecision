48b8599464412883176c4bd64c18d0ecc6901468
David Henley <henleydr@gmail.com>
2017-11-27 17:15:50 -0600

    css and style loaders

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
 
+import './styles/styles.css';
+
 import App from './components/App';
 
 render(<App />, document.querySelector('#root'))
